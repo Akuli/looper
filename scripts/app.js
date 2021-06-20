a69e753d69df9e63bfd01f20044d5ca5be001f4a
Akuli <akuviljanen17@gmail.com>
2021-06-20 14:40:58 +0300

    kinda works but lets say it has some problems

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,81 @@
-async function main() {
-  const record = document.getElementById('record');
-  const stop = document.getElementById('stop');
-  stop.disabled = true;
+let ctx = null;
 
-  userMedia = await navigator.mediaDevices.getUserMedia({audio: true});
+async function blobToArrayBuffer(blob) {
+  // https://stackoverflow.com/a/40364457
+  const fileReader = new FileReader();
+  await new Promise(resolve => {
+    fileReader.onloadend = resolve;
+    fileReader.readAsArrayBuffer(blob);
+  });
+  return fileReader.result;
+}
 
-  /*
-  const mediaRecorder = new MediaRecorder(userMedia);
-  const looperState = {
-    chunks: [],
-    blob: null,
-  };
+async function arrayBufferToAudioBuffer(arrayBuffer) {
+  return await new Promise(resolve => ctx.decodeAudioData(arrayBuffer, resolve));
+}
 
-  record.onclick = function() {
-    mediaRecorder.start();
-    stop.disabled = false;
-    record.disabled = true;
-  }
-
-  stop.onclick = function() {
-    mediaRecorder.stop();
-    stop.disabled = true;
-    record.disabled = false;
-  }
-
-  mediaRecorder.onstop = function(e) {
-    looperState.audio = document.createElement('audio');
-    document.body.appendChild(looperState.audio);
-
-    looperState.blob = new Blob(looperState.chunks, { type: 'audio/ogg; codecs=opus' });
-    looperState.audio.src = window.URL.createObjectURL(looperState.blob);
-    looperState.audio.loop = true;
-    looperState.audio.play();
-  }
-
-  mediaRecorder.ondataavailable = (e => looperState.chunks.push(e.data));
-  */
-
-  const ctx = new AudioContext();
-  const microphone = ctx.createMediaStreamSource(userMedia);
-  microphone.connect(ctx.destination);
-
-  const oscillator = ctx.createOscillator();
-  oscillator.frequency.setValueAtTime(110*Math.pow(2,2/12), ctx.currentTime);
-
-  var gainNode = ctx.createGain();
-  gainNode.gain.value = 0.01;
-
-  oscillator.connect(gainNode);
-  gainNode.connect(ctx.destination)
-  oscillator.start();
-
-  // TODO https://developer.mozilla.org/en-US/docs/Web/API/AudioBufferSourceNode
-
+function createTestAudioBuffer() {
   // One channel, two seconds
   const audioBuffer = ctx.createBuffer(1, ctx.sampleRate * 2, ctx.sampleRate);
-  console.log(audioBuffer);
 
   const float32array = audioBuffer.getChannelData(0);
   for (let i = 0; i < audioBuffer.length; i++) {
     let note;
     if (i % ctx.sampleRate < 0.25*ctx.sampleRate) {
-      note = 6;
+      note = 7;
     } else if (i % ctx.sampleRate < 0.5*ctx.sampleRate) {
-      note = 9;
+      note = 10;
     } else if (i % ctx.sampleRate < 0.75*ctx.sampleRate) {
-      note = 14;
+      note = 15;
     } else {
-      note = 18;
+      note = 19;
     }
     float32array[i] = Math.sin(2*Math.PI*110*Math.pow(2, note/12)*i/ctx.sampleRate);
   }
 
-  var bufSource = ctx.createBufferSource();
-  bufSource.buffer = audioBuffer;
-  bufSource.loop = true;
-  bufSource.connect(gainNode);
-  bufSource.start();
+  return audioBuffer;
+}
+
+async function main() {
+  const recordButton = document.getElementById('record');
+  const stopButton = document.getElementById('stop');
+
+  const userMedia = await navigator.mediaDevices.getUserMedia({audio: true});
+  ctx = new AudioContext();
+
+  const streamDestination = ctx.createMediaStreamDestination();
+  const microphone = ctx.createMediaStreamSource(userMedia);
+  microphone.connect(streamDestination);
+
+  var gainNode = ctx.createGain();
+  gainNode.gain.value = 1;
+  gainNode.connect(ctx.destination)
+
+  let mediaRecorder;
+
+  recordButton.addEventListener('click', () => {
+    mediaRecorder = new MediaRecorder(streamDestination.stream)
+    const chunks = [];
+    mediaRecorder.ondataavailable = event => chunks.push(event.data);
+    mediaRecorder.onstop = async () => {
+      const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
+      console.log(blob);
+/*      const url = URL.createObjectURL(blob);
+      console.log(url);
+      document.body.innerHTML += `<audio controls src="${url}"></iframe>`*/
+
+      var bufSource = ctx.createBufferSource();
+      bufSource.buffer = await arrayBufferToAudioBuffer(await blobToArrayBuffer(blob));
+      bufSource.loop = true;
+      bufSource.connect(gainNode);
+      bufSource.start();
+    };
+    mediaRecorder.start();
+  });
+
+  stopButton.addEventListener('click', () => {
+    mediaRecorder.requestData();
+    mediaRecorder.stop();
+  });
 }
 main()

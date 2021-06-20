41e621e9d24958a8b327f30abc890f97bf53f40a
Akuli <akuviljanen17@gmail.com>
2021-06-20 18:12:16 +0300

    pad all layers to same duration, and combine them properly

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,7 @@
+let loopDuration = 2;
 let ctx = null;
+let loopAudioBuffer = null;
+let chanCounter = 0;
 
 async function blobToArrayBuffer(blob) {
   // https://stackoverflow.com/a/40364457
@@ -11,12 +14,13 @@ async function blobToArrayBuffer(blob) {
 }
 
 async function arrayBufferToAudioBuffer(arrayBuffer) {
-  return await new Promise(resolve => ctx.decodeAudioData(arrayBuffer, resolve));
+  const audioBuffer = await new Promise(resolve => ctx.decodeAudioData(arrayBuffer, resolve));
+  return audioBuffer;
 }
 
 function createTestAudioBuffer() {
   // One channel, two seconds
-  const audioBuffer = ctx.createBuffer(1, ctx.sampleRate * 2, ctx.sampleRate);
+  const audioBuffer = ctx.createBuffer(1, ctx.sampleRate * loopDuration, ctx.sampleRate);
 
   const float32array = audioBuffer.getChannelData(0);
   for (let i = 0; i < audioBuffer.length; i++) {
@@ -36,39 +40,57 @@ function createTestAudioBuffer() {
   return audioBuffer;
 }
 
+function connectMultiChannelToSpeaker(source) {
+  const splitter = ctx.createChannelSplitter(32);
+  source.connect(splitter);
+
+  const merger = ctx.createChannelMerger(2);
+  for (let chanNumber = 0; chanNumber < 32; chanNumber++) {
+    // Every channel goes to left and right
+    splitter.connect(merger, chanNumber, 0);
+    splitter.connect(merger, chanNumber, 1);
+  }
+  merger.connect(ctx.destination);
+}
+
 async function main() {
   const recordButton = document.getElementById('record');
   const stopButton = document.getElementById('stop');
 
   const userMedia = await navigator.mediaDevices.getUserMedia({audio: true});
-  ctx = new AudioContext();
+  ctx = new AudioContext({ sampleRate: 44100 });
+  loopAudioBuffer = new AudioBuffer({ length: 2*ctx.sampleRate, sampleRate: ctx.sampleRate, numberOfChannels: 32 });
+  for (let i = 0; i < 32; i++) {
+    loopAudioBuffer.getChannelData(i).fill(0);
+  }
 
   const streamDestination = ctx.createMediaStreamDestination();
   const microphone = ctx.createMediaStreamSource(userMedia);
   microphone.connect(streamDestination);
 
-  var gainNode = ctx.createGain();
-  gainNode.gain.value = 1;
-  gainNode.connect(ctx.destination)
-
   let mediaRecorder;
+  const bufSource = ctx.createBufferSource();
+  bufSource.channelCount = 32;
+  bufSource.buffer = loopAudioBuffer;
+  console.log("bufSource", bufSource);
+  console.log("bufSource.channelCount", bufSource.channelCount);
+  bufSource.loop = true;
+  connectMultiChannelToSpeaker(bufSource);
+  bufSource.start();
 
   recordButton.addEventListener('click', () => {
     mediaRecorder = new MediaRecorder(streamDestination.stream)
     const chunks = [];
     mediaRecorder.ondataavailable = event => chunks.push(event.data);
     mediaRecorder.onstop = async () => {
-      const blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
-      console.log(blob);
-/*      const url = URL.createObjectURL(blob);
-      console.log(url);
-      document.body.innerHTML += `<audio controls src="${url}"></iframe>`*/
-
-      var bufSource = ctx.createBufferSource();
-      bufSource.buffer = await arrayBufferToAudioBuffer(await blobToArrayBuffer(blob));
-      bufSource.loop = true;
-      bufSource.connect(gainNode);
-      bufSource.start();
+      const audioBuffer = await arrayBufferToAudioBuffer(
+        await blobToArrayBuffer(
+          new Blob(chunks, { type: 'audio/ogg; codecs=opus' })
+        )
+      );
+      const i = chanCounter++;
+      console.log("Writing to channel " + i);
+      loopAudioBuffer.getChannelData(i).set(audioBuffer.getChannelData(0), 0);
     };
     mediaRecorder.start();
   });
